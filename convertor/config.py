95be448fbf7eeb88224454bf47e6a7cba79a4dae
lager <machv@cesnet.cz>
2018-10-25 11:43:20 +0200

    update config

--- convertor/config.py
+++ convertor/config.py
@@ -2,7 +2,8 @@
 # ==============================================================================
 # configuration file for eduroam-db convertor 
 # ==============================================================================
-ROid = "TODO"
-default_lang = "cs"
-default_contact_privacy = 0     # 0 - private, 1 - public
-default_contact_type = 0        # 0 - person, 1 - service/department
+ROid = "CZ01"                      # ROid provided by database operator
+default_lang = "cs"                # default lang used in country where the NRO is managing eduroam
+default_contact_privacy = 0        # 0 - private (default), 1 - public
+default_contact_type = 0           # 0 - person (default), 1 - service/department
+local_timezone = "Europe/Prague"   # time zone of the NRO country
