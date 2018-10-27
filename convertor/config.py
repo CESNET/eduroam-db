12e9bc6957a52a59edb27ab4ffc02073bb95cec4
lager <machv@cesnet.cz>
2018-10-27 12:30:52 +0200

    update config

--- convertor/config.py
+++ convertor/config.py
@@ -7,3 +7,9 @@ default_lang = "cs"                # default lang used in country where the NRO 
 default_contact_privacy = 0        # 0 - private (default), 1 - public
 default_contact_type = 0           # 0 - person (default), 1 - service/department
 local_timezone = "Europe/Prague"   # time zone of the NRO country
+default_stage = 1                  # 0 - preproduction/test, 1 - active
+# ==============================================================================
+# option specific configuration
+# ==============================================================================
+lon_values = list(range(48, 52, 1))     # values between [48,51] are accepted as longtitude
+lat_values = list(range(11, 19, 1))     # values between [11,18] are accepted as latitude
