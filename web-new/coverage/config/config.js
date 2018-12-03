1abdd1118b2c381e09002688901715083827eab3
lager <machv@cesnet.cz>
2018-12-03 15:55:34 +0100

    add config file

---
+++ web-new/coverage/config/config.js
@@ -0,0 +1,10 @@
+// confoguration file
+// ==========================================================================================
+module.exports = {
+  ldap_host            : 'ldap.cesnet.cz',
+  bind_dn              : 'uid=monitor.eduroam.cz,ou=special users,dc=cesnet,dc=cz',
+  search_base_realms   : 'ou=Realms,o=eduroam,o=apps,dc=cesnet,dc=cz',
+  search_base_people   : 'ou=People,dc=cesnet,dc=cz'
+}
+// ==========================================================================================
+
