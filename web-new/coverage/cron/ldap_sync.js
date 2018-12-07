d3453f95236991005580a4626804784b83df74bd
lager <machv@cesnet.cz>
2018-12-07 23:18:58 +0100

    remove solved TODO

--- web-new/coverage/cron/ldap_sync.js
+++ web-new/coverage/cron/ldap_sync.js
@@ -122,7 +122,7 @@ function prepare_data(data)
     }
 
     if(data[i].org_ptr)
-      ret.realm_to_inst[data[i].realm] = data[i].org_ptr.replace("dc=", "").replace(",ou=Organizations,dc=cesnet,dc=cz", "").replace(" ", "_");   // TODO - mezera za _ je spravne?
+      ret.realm_to_inst[data[i].realm] = data[i].org_ptr.replace("dc=", "").replace(",ou=Organizations,dc=cesnet,dc=cz", "").replace(" ", "_");
   }
 
   generate_config(ret);
