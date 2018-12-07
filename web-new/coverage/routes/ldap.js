5353a33b78214c340e48713dd184009af99917d5
lager <machv@cesnet.cz>
2018-12-07 22:28:40 +0100

    implement getting institution info

--- web-new/coverage/routes/ldap.js
+++ web-new/coverage/routes/ldap.js
@@ -2,6 +2,7 @@ const ldap = require('ldapjs');
 const config = require('../config/config.js');
 const secrets = require('../config/secrets.js');
 const assert = require('assert');
+const async = require('async');
 // --------------------------------------------------------------------------------------
 var exp = {}
 // --------------------------------------------------------------------------------------
@@ -56,21 +57,20 @@ function get_user_realms(client, user, response, callback)
 // --------------------------------------------------------------------------------------
 // get institution's data by ldap dn
 // --------------------------------------------------------------------------------------
-function get_inst_data(client, inst, response)
+function get_inst_data(client, data, response)
 {
   var items = [ 'street', 'l', 'o', 'o;lang-cs' ];      // TODO ?
-  var ret;
 
   var opts = {
     scope: 'sub',
     attributes: items
   };
 
-  client.search(inst, opts, function(err, res) {
+  client.search(data.o_pointer, opts, function(err, res) {
     assert.ifError(err);
 
     res.on('searchEntry', function(entry) {
-      ret = entry.object;
+      data.inst_details = entry.object;
     });
 
     res.on('error', function(err) {
@@ -82,8 +82,127 @@ function get_inst_data(client, inst, response)
         assert.ifError(err);
       });
 
-      response.send(ret);
+      response.send(create_institituon_json(data));       // TODO - upravit strukturu tak, aby odpovidaja JSON souborum?
     });
+  });
+}
+// --------------------------------------------------------------------------------------
+// create empty institution.json structure with ldap provided data
+// --------------------------------------------------------------------------------------
+function create_institituon_json(data)
+{
+
+  var ret = {
+    "policy_URL": [
+      {
+        "data": "http://www.eduroam.cz/doku.php?id=cs:roamingova_politika",
+        "lang": "cs"
+      }
+    ],
+    "stage": 1,
+    "contact": [
+    ],
+    "address": [
+    ],
+    "type": data.type,           // [ IdPSP, SP, IdP ]
+    "location": [
+    ],
+    "inst_name": [
+    ],
+    "inst_realm": [
+    ],
+    "info_URL": [
+    ],
+    "instid": "cesnet.cz",          // TODO - jak se tohle vlastne generuje?        cn z organizace? v souborech je aktualne realm - hm? asi jsem vzal jina data nez z konverze edudb-cache?
+    "ROid": "CZ01"
+  };
+
+  // --------------------------------------------------------------------------------------
+
+  for(var i in data.manager_details) {
+    if(typeof(data.manager_details[i].mail) === 'object')       // mupltiple addresses
+      var contact = { "type": 0, "privacy": 0, "email": data.manager_details[i].mail[0], "name": data.manager_details[i]['cn;lang-cs'] };
+    else        // one mail only
+      var contact = { "type": 0, "privacy": 0, "email": data.manager_details[i].mail, "name": data.manager_details[i]['cn;lang-cs'] };
+
+    ret.contact.push(contact);
+  }
+
+  // --------------------------------------------------------------------------------------
+
+  ret.address.push(
+    {
+      "city": {
+        "data": data.inst_details.l,
+        "lang": "cs"
+      },
+      "street": {
+        "data": data.inst_details.street,
+        "lang": "cs"
+      }
+    });
+
+  ret.address.push(
+    {
+      "city": {
+        "data": data.inst_details.l,
+        "lang": "en"
+      },
+      "street": {
+        "data": data.inst_details.street,
+        "lang": "en"
+      }
+    });
+
+  // --------------------------------------------------------------------------------------
+
+  ret.inst_name.push({ "data": data.inst_details.o, "lang": "cs" });
+  ret.inst_name.push({ "data": data.inst_details.o, "lang": "en" });
+
+  // --------------------------------------------------------------------------------------
+
+  if(typeof(data.realm_list) === 'object') {
+    for(var i in data.realm_list)
+      ret.inst_realm.push(data.realm_list[i]);
+  }
+  else
+    ret.inst_realm.push(data.realm_list);
+
+  // --------------------------------------------------------------------------------------
+
+  return ret;
+}
+// --------------------------------------------------------------------------------------
+// get managers for specific for specific realm by dn
+// --------------------------------------------------------------------------------------
+function get_managers(client, ret, done)
+{
+  var items = [ 'cn', 'mail' ];         // TODO - phone number?
+  ret.manager_details = [];
+
+  var opts = {
+    scope: 'sub',
+    attributes: items
+  };
+
+  async.eachOfSeries(ret.managers, function(item, key, callback) {
+    client.search(item, opts, function(err, res) {      // search for manager
+      assert.ifError(err);
+
+      res.on('searchEntry', function(entry) {
+        ret.manager_details.push(entry.object);
+      });
+
+      res.on('error', function(err) {
+        console.error('error: ' + err.message);
+      });
+
+      res.on('end', function(result) {
+        callback();
+      });
+    });
+  }, function() {
+    done();         // all done, continue processing
   });
 }
 // --------------------------------------------------------------------------------------
@@ -91,8 +210,9 @@ function get_inst_data(client, inst, response)
 // --------------------------------------------------------------------------------------
 function get_inst(client, realm, response)
 {
-  var items = [ 'oPointer' ];
-  var ret;
+  var items = [ 'oPointer', 'manager', 'eduroamMemberType', 'cn' ];
+  var ret = {};
+  ret.managers = [];
 
   var opts = {
     filter: 'cn=' + realm,
@@ -104,7 +224,14 @@ function get_inst(client, realm, response)
     assert.ifError(err);
 
     res.on('searchEntry', function(entry) {
-      ret = entry.object.oPointer;
+      ret.o_pointer = entry.object.oPointer;
+      ret.type = entry.object.eduroamMemberType;
+      ret.realm_list = entry.object.cn;
+
+      if(typeof(entry.object.manager) === 'object')
+        ret.managers = object.entry.manager;
+      else
+        ret.managers.push(entry.object.manager);
     });
 
     res.on('error', function(err) {
@@ -112,7 +239,9 @@ function get_inst(client, realm, response)
     });
 
     res.on('end', function(result) {
-      get_inst_data(client, ret, response);
+      get_managers(client, ret, function() {
+        get_inst_data(client, ret, response);
+      });
     });
   });
 }
