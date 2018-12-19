394bf38ba098a122170ef97becd5a675acb29e9a
lager <machv@cesnet.cz>
2018-12-19 14:15:52 +0100

    use different user for ldap access

--- web-new/coverage/config/config.js
+++ web-new/coverage/config/config.js
@@ -2,7 +2,7 @@
 // ==========================================================================================
 module.exports = {
   ldap_host            : 'ldap.cesnet.cz',
-  bind_dn              : 'uid=monitor.eduroam.cz,ou=special users,dc=cesnet,dc=cz',
+  bind_dn              : 'uid=pokryti.eduroam.cz,ou=special users,dc=cesnet,dc=cz',
   search_base_realms   : 'ou=Realms,o=eduroam,o=apps,dc=cesnet,dc=cz',
   search_base_people   : 'ou=People,dc=cesnet,dc=cz'
 }
