80c484da4dea6d2c962389bb79a768323ea427f8
lager <machv@cesnet.cz>
2019-01-05 00:15:59 +0100

    remove webpack config

--- web/coverage/webpack.config.js
+++
@@ -1,9 +0,0 @@
-const webpack = require('webpack');
-
-module.exports = {
-  entry: './javascripts/angular/controllers.js',
-  output: {
-    path: __dirname,
-    filename: 'controllers.webpacked.js'
-  },
-};
