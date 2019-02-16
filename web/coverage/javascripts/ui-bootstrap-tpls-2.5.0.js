43bcd2e9fce89aee65c354212d907bd3942a3ce2
lager <machv@cesnet.cz>
2019-02-16 02:18:15 +0100

    add visual expansion hint symbol to accordion

--- web/coverage/javascripts/ui-bootstrap-tpls-2.5.0.js
+++ web/coverage/javascripts/ui-bootstrap-tpls-2.5.0.js
@@ -7417,7 +7417,7 @@ angular.module("uib/template/accordion/accordion-group.html", []).run(["$templat
   $templateCache.put("uib/template/accordion/accordion-group.html",
     "<div role=\"tab\" id=\"{{::headingId}}\" aria-selected=\"{{isOpen}}\" class=\"panel-heading\" ng-keypress=\"toggleOpen($event)\">\n" +
     "  <h4 class=\"panel-title\">\n" +
-    "    <a role=\"button\" data-toggle=\"collapse\" aria-expanded=\"{{isOpen}}\" aria-controls=\"{{::panelId}}\" tabindex=\"0\" class=\"accordion-toggle\" ng-click=\"toggleOpen()\" uib-accordion-transclude=\"heading\" ng-disabled=\"isDisabled\" uib-tabindex-toggle><span uib-accordion-header ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span></a>\n" +
+    "    <a role=\"button\" data-toggle=\"collapse\" aria-expanded=\"{{isOpen}}\" aria-controls=\"{{::panelId}}\" tabindex=\"0\" class=\"accordion-toggle\" ng-click=\"toggleOpen()\" uib-accordion-transclude=\"heading\" ng-disabled=\"isDisabled\" uib-tabindex-toggle><span class=\"glyphicon glyphicon-menu-down\"></span><span uib-accordion-header ng-class=\"{'text-muted': isDisabled}\">{{heading}}</span></a>\n" +
     "  </h4>\n" +
     "</div>\n" +
     "<div id=\"{{::panelId}}\" aria-labelledby=\"{{::headingId}}\" aria-hidden=\"{{!isOpen}}\" role=\"tabpanel\" class=\"panel-collapse collapse\" uib-collapse=\"!isOpen\" collapsed=\"collapsed()\" collapsing=\"collapsing()\" expanded=\"expanded()\" expanding=\"expanding()\">" +
