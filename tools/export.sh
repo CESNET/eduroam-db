2809abaa894494bd026ebc4ca072b3a65c232e60
lager <machv@cesnet.cz>
2019-03-11 15:01:09 +0100

    create list of connected institutions on export

--- tools/export.sh
+++ tools/export.sh
@@ -7,6 +7,7 @@ function main()
   /home/eduroamdb/eduroam-db/web/coverage/ldap_check.js # check JSON data with ldap - type and realms must match
   /home/eduroamdb/eduroam-db/tools/inst_json.sh $coverage_files_path $institution_json_output    # create institution.json
   /home/eduroamdb/eduroam-db/maps/map_data.sh $institution_json_output > $geojson   # create geojson data from institution.json for eduroam.cz maps
+  /home/eduroamdb/edudb.sh >/dev/null 2>&1      # create list of connected institutions for eduroam.cz
 }
 # ======================================================================
 coverage_files_path="/home/eduroamdb/eduroam-db/web/coverage/coverage_files"
