9500aab706d2642737fa2fb28c2c62bb54a6ec18
lager <machv@cesnet.cz>
2020-02-19 18:42:24 +0100

    update config & ldap sync

--- web/coverage/config/config.js
+++ web/coverage/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   ldap_host            : 'ldap.cesnet.cz',
   bind_dn              : 'uid=pokryti.eduroam.cz,ou=special users,dc=cesnet,dc=cz',
   search_base_realms   : 'ou=Realms,o=eduroam,o=apps,dc=cesnet,dc=cz',
-  search_base_people   : 'ou=People,dc=cesnet,dc=cz'
+  search_base_people   : 'ou=People,o=eduroam,o=apps,dc=cesnet,dc=cz'
 }
 // ==========================================================================================
 
--- web/coverage/cron/ldap_sync.js
+++ web/coverage/cron/ldap_sync.js
@@ -62,7 +62,45 @@ exp.search_realms = function (client, search_base, callback)
 
     res.on('error', function(err) {
       console.error('error: ' + err.message);
-      callback();
+      if(callback)
+        callback();
+    });
+
+    res.on('end', function(result) {
+      if(callback)
+        search_admins(client, config.search_base_people, ret, callback)
+      else
+        search_admins(client, config.search_base_people, ret)
+    });
+  });
+}
+// --------------------------------------------------------------------------------------
+// search admins
+// --------------------------------------------------------------------------------------
+function search_admins(client, search_base, data, callback)
+{
+  // items which are registered for each realm
+  var items = [ 'edupersonuniqueid', 'dn' ];
+  var ret = {};
+
+  var opts = {
+    filter: '(objectclass=*)',
+    scope: 'sub',
+    attributes: items
+  };
+
+  client.search(search_base, opts, function(err, res) {
+    assert.ifError(err);
+
+    res.on('searchEntry', function(entry) {
+      if(entry.object['edupersonuniqueid'])
+        ret[entry.object['dn'].toLowerCase()] = entry.object['edupersonuniqueid'];
+    });
+
+    res.on('error', function(err) {
+      console.error('error: ' + err.message);
+      if(callback)
+        callback();
     });
 
     res.on('end', function(result) {
@@ -70,7 +108,7 @@ exp.search_realms = function (client, search_base, callback)
         assert.ifError(err);
       });
 
-      prepare_data(ret);
+      prepare_data(data, ret);
 
       if(callback)
         callback();
@@ -93,29 +131,29 @@ function sort_by_domain(a, b)
 // --------------------------------------------------------------------------------------
 // prepare data in needed form
 // --------------------------------------------------------------------------------------
-function prepare_data(data)
+function prepare_data(realms, managers)
 {
   var ret = {};
   ret.admin_to_realm = {};
   ret.realm_to_inst = {};
   var tmp_realms = [];
 
-  for(var i in data) {
-    tmp_realms.push(data[i].realm);
+  for(var i in realms) {
+    tmp_realms.push(realms[i].realm);
 
-    for(var j in data[i].managers) {
-      data[i].managers[j] = data[i].managers[j].replace("uid=", "").replace(",ou=people,dc=cesnet,dc=cz", "@cesnet.cz");
+    for(var j in realms[i].managers) {
+      realms[i].managers[j] = managers[realms[i].managers[j]];
 
-      if(ret.admin_to_realm[data[i].managers[j]])
-        ret.admin_to_realm[data[i].managers[j]].push(data[i].realm);
+      if(ret.admin_to_realm[realms[i].managers[j]])
+        ret.admin_to_realm[realms[i].managers[j]].push(realms[i].realm);
       else {
-        ret.admin_to_realm[data[i].managers[j]] = [];
-        ret.admin_to_realm[data[i].managers[j]].push(data[i].realm);
+        ret.admin_to_realm[realms[i].managers[j]] = [];
+        ret.admin_to_realm[realms[i].managers[j]].push(realms[i].realm);
       }
     }
 
-    if(data[i].org_ptr)
-      ret.realm_to_inst[data[i].realm] = data[i].org_ptr.replace("dc=", "").replace(",ou=Organizations,dc=cesnet,dc=cz", "").replace(/ /g, "_");
+    if(realms[i].org_ptr)
+      ret.realm_to_inst[realms[i].realm] = realms[i].org_ptr.replace("dc=", "").replace(",ou=Organizations,dc=cesnet,dc=cz", "").replace(/ /g, "_");
   }
 
   ret.realms = tmp_realms.sort(sort_by_domain);
